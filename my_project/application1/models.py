30a9c4c3e16fc72e300890ebb6b3cecf1bbe941d
Bogdan <bogdanatrosenko@gmail.com>
2024-01-30 22:26:49 +0300

    now you can save your files on the server using django admin

--- my_project/application1/models.py
+++ my_project/application1/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+def user_directory_path_by_images(instance, filename):
+    """Путь по которому сохраняются изображения на сервере"""
+    return f'user_{instance.user.id}/images/{datetime.now().strftime("%Y-%m-%d")}/{filename}'
+
+
+def user_directory_path_by_files(instance, filename):
+    """Путь по которому сохраняются файлы на сервере"""
+    return f'user_{instance.user.id}/files/{datetime.now().strftime("%Y-%m-%d")}/{filename}'
+
+
+class Any_file(models.Model):
+    """Модель для хранения файла/изображения + некое общее имя"""
+    user = models.ForeignKey(to=User, null=True, on_delete=models.SET_NULL)
+    some_name = models.CharField(max_length=50, unique=True)
+    image = models.ImageField(upload_to=user_directory_path_by_images)
+    file = models.FileField(upload_to=user_directory_path_by_files)
+
+    def __str__(self):
+        return self.some_name
